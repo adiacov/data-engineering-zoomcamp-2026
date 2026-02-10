9fe4b53dcc3d4eee4f4560e55f5b68ecc11ca463
adiacov <diacovmd@gmail.com>
2026-02-10 15:54:59 +0200

    004-analytics-engineering - start module; setup dbt project

---
+++ modules/004-analytics-engineering/logs/query_log.sql
@@ -0,0 +1,9 @@
+-- created_at: 2026-02-13T09:41:13.493783693+00:00
+-- finished_at: 2026-02-13T09:41:15.952135262+00:00
+-- elapsed: 2.5s
+-- outcome: success
+-- dialect: bigquery
+-- node_id: not available
+-- query_id: BrZSfw0cDsjffFza0iAN90iHJyT
+-- desc: not provided
+select 1 as id;
